fb70dd760f60635aa0c618c6f8d5de9029ce80df
신재선 <@Shin-jaeseon>
2018-04-30 10:08:25 +0900

    1.2.1

--- src/com/example/GitTest.java
+++ src/com/example/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 			System.out.println("asdasd");
 			System.out.println("메세지 확인 완료");
+			System.out.println("아 개 시 끄 러 워");
 			
 	}
 

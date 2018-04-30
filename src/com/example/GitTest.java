eff27c6fd2fb7a9aac0cb82bcf9f5850f86b81ff
djaak <djaak@DESKTOP-JTMOPP4>
2018-04-30 10:34:31 +0900

    final

--- src/com/example/GitTest.java
+++ src/com/example/GitTest.java
@@ -4,8 +4,6 @@ public class GitTest {
 
 	public static void main(String[] args) {
 			System.out.println("asdasd");
-			System.out.println("메세지 확인 완료");
-			System.out.println("아 개 시 끄 러 워");
-			System.out.println("팀장 작업3");
+			System.out.println("last version");
 	}
 }
